db6f73aec775afe5ca583d7bc5a8ccd2abc858d0
mikelojeda25 <112379028+mikelojeda25@users.noreply.github.com>
2026-04-11 05:23:02 +0800

    feat: Pagination under index.php

--- wp-content/themes/jotdown-theme/index.php
+++ wp-content/themes/jotdown-theme/index.php
@@ -9,10 +9,18 @@
       <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
       <div><?php the_content(); ?></div>
     </article>
-
     <?php endwhile; else : ?>
       <p>No post available.</p>
     <?php endif; ?>
+
+    <div class="pagination">
+        <?php 
+            echo paginate_links(array(
+                'prev_text' => '&laquo; Prev',
+                'next_text' => 'Next &raquo;', // Dagdagan mo ng semicolon dito sa dulo ng raquo
+            ));
+        ?>
+    </div>
   </main>
 
   <aside style="flex: 1;">
